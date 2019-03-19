0822d0185f8d62e44ddb7267786e1898fcb3bacf
Fabian Trinler <fabian.trinler@blateral.com>
2019-03-19 09:52:11 +0100

    Fixining storybook webpack config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,14 +1,11 @@
 const path = require('path');
 
-module.exports = (baseConfig, env, config) => {
+module.exports = ({ config }) => {
     config.module.rules.push({
         test: /\.tsx?$/,
         include: path.resolve(__dirname, '../src'),
         exclude: /node_modules/,
         use: [
-            {
-                loader: require.resolve('babel-loader'),
-            },
             {
                 loader: require.resolve('ts-loader'),
             },
