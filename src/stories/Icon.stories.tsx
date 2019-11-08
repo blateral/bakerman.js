3b6d1f7a03b2a39513d31aaf39b71ab671700e2b
Alexander Rieß <alexander.riess@blateral.com>
2019-11-08 15:39:20 +0100

    Adding icon system

---
+++ src/stories/Icon.stories.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import Icon from '../components/base/Icon';
+
+storiesOf('Base / Icon', module)
+    .add('angle-bottom', () => <Icon type="angle-bottom" />)
+    .add('angle-left', () => <Icon type="angle-left" />)
+    .add('angle-right', () => <Icon type="angle-right" />)
+    .add('angle-top', () => <Icon type="angle-top" />);
